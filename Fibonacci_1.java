47e8d9af89797e74cf29d640e78a1c85a50c1c3f
SenthilKumar0201 <senthilpg15@gmail.com>
2024-09-09 20:19:24 +0530

    Initial commit

---
+++ Fibonacci_1.java
@@ -0,0 +1,19 @@
+package Java_Package;
+
+public class Fibonacci_1 {
+
+public void Fib()
+{
+	int a=0;
+	int b=1;
+	int c=0;
+		for(int i=1;i<=7;i++)
+		{
+		a=b;
+		b=c;
+		//c=a+b;
+		System.out.println("My Fibnoci is" + ":" + c);
+		c=a+b;
+		}
+}
+}
---
+++ Main_Fibonacci.java
@@ -0,0 +1,12 @@
+package Java_Package;
+
+public class Main_Fibonacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Fibonacci_1 sk=new Fibonacci_1();
+		sk.Fib();
+	}
+	
+	
+}
